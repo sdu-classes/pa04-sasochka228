e3b7477366d54b13cc7ef5b1540f20c1bc6935fc
sasochka228 <124434106+sasochka228@users.noreply.github.com>
2023-02-24 17:31:19 +0600

    Update MovableCircle.java

--- problem-A/MovableCircle.java
+++ problem-A/MovableCircle.java
@@ -11,22 +11,22 @@ public class MoveableCircle implements Movable{
         return String.format("(%1$s, %2$s), speed=(%3$s, %4$s), radius=%4$s" ,
                 center.x, center.y, center.xSpeed,center.ySpeed, radius1);}
 
-
+    @Override
     public void moveUp() {center.moveUp();
 
     }
 
-
+    @Override
     public void moveDown() {center.moveDown();
 
     }
 
-
+    @Override
     public void moveLeft() {center.moveLeft();
 
     }
 
-
+    @Override
     public void moveRight() {center.moveRight();
 
     }
