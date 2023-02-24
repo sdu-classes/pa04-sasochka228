97a6d172266438fede680f784ba3df5c611fc580
sasochka228 <124434106+sasochka228@users.noreply.github.com>
2023-02-24 22:56:58 +0600

    Update Movable.java

--- problem-A/Movable.java
+++ problem-A/Movable.java
@@ -1,7 +1,20 @@
-public interface Movable {
-    void moveUp();
-    void moveDown();
-    void moveLeft();
-    void moveRight();
+abstract interface Movaable {
+    abstract void moveUp();
+    abstract void moveDown();
+    abstract void moveLeft();
+    abstract void moveRight();
 
+}
+
+public class Movable{
+    public static void main(String[] args) {
+        MovablePoint mv = new MovablePoint(10,10,1,1);
+        mv.moveDown();
+        mv.moveRight();
+        System.out.println(mv.toString());
+        MovableCircle mc = new MovableCircle(10,10,1,1,5);
+        mc.moveUp();
+        mc.moveLeft();
+        System.out.println(mc.toString());
+    }
 }
