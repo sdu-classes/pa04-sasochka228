f858b21c67303e8248fbe9e9d7e94d86c9b21db7
sasochka228 <124434106+sasochka228@users.noreply.github.com>
2023-02-24 22:47:43 +0600

    Update MovablePoint.java

--- problem-A/MovablePoint.java
+++ problem-A/MovablePoint.java
@@ -1,33 +1,33 @@
-public class MovaeblePoint {
-    public int x ;
-    public int y ;
+public class MovablePoint {
+    public int x;
+    public int y;
     public int xSpeed;
     public int ySpeed;
 
-    MovaeblePoint(int x , int y , int xSpeed, int ySpeed){
-        this.x=x;
-        this.y=y;
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
     }
-    public String toString(){
-        return "("+this.x + "," + this.y + "), speed =("+this.xSpeed + ","+this.ySpeed+")";}
-        
-    public void moveUp(){
+
+    public String toString() {
+        return "(" + this.x + "," + this.y + "), speed =(" + this.xSpeed + "," + this.ySpeed + ")";
+    }
+
+    public void moveUp() {
         y -= ySpeed;
     }
-    public void moveDown(){
+
+    public void moveDown() {
         y += ySpeed;
-
     }
-    public void moveLeft(){
+
+    public void moveLeft() {
         x -= xSpeed;
-
     }
-    public void moveRight(){
+
+    public void moveRight() {
         x += xSpeed;
-
     }
-
 }
-
