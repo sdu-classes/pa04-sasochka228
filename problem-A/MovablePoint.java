e4b6b1303511bdc015a2b930e3082e77a81715e0
sasochka228 <124434106+sasochka228@users.noreply.github.com>
2023-02-24 23:05:46 +0600

    Update MovablePoint.java

--- problem-A/MovablePoint.java
+++ problem-A/MovablePoint.java
@@ -1,33 +1,32 @@
-public class MovableCircle implements Movaable {
-    MovablePoint center;
-    int radius;
+public class MovaeblePoint {
+    public int x ;
+    public int y ;
+    public int xSpeed;
+    public int ySpeed;
 
-    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
-        this.center = new MovablePoint(x, y, xSpeed, ySpeed);
-        this.radius = radius;
+    MovaeblePoint(int x , int y , int xSpeed, int ySpeed){
+        this.x=x;
+        this.y=y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
     }
-
-    @Override
-    public void moveUp() {
-        center.moveUp();
-    }
-
-    @Override
-    public void moveDown() {
-        center.moveDown();
-    }
-
-    @Override
-    public void moveLeft() {
-        center.moveLeft();
-    }
-
-    @Override
-    public void moveRight() {
-        center.moveRight();
-    }
-
     public String toString(){
-        return "("+center.x + "," + center.y + "), speed =("+center.xSpeed + ","+center.ySpeed+"), radius = "+radius;
+        return "("+this.x + "," + this.y + "), speed =("+this.xSpeed + ","+this.ySpeed+")";}
+
+    public void moveUp(){
+        y -= ySpeed;
     }
+    public void moveDown(){
+        y += ySpeed;
+
+    }
+    public void moveLeft(){
+        x -= xSpeed;
+
+    }
+    public void moveRight(){
+        x += xSpeed;
+
+    }
+
 }
