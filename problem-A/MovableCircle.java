60c7fc1789ca41e29c7c8ebaf65c4bfe8b64071c
sasochka228 <124434106+sasochka228@users.noreply.github.com>
2023-02-24 17:26:26 +0600

    Update MovableCircle.java

--- problem-A/MovableCircle.java
+++ problem-A/MovableCircle.java
@@ -1,33 +1,33 @@
-public class MoveableCircle implements Movable{
-    private int radius1;
+public class MovableCircle implements Movaable {
+    MovablePoint center;
+    int radius;
 
-    private MovaeblePoint center;
-
-    MoveableCircle(int x , int y ,int xSpeed , int ySpeed,int radius){
-        center = new MovaeblePoint(x,y,xSpeed,ySpeed);
-        radius1=radius;
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
     }
+
+    @Override
+    public void moveUp() {
+        center.moveUp();
+    }
+
+    @Override
+    public void moveDown() {
+        center.moveDown();
+    }
+
+    @Override
+    public void moveLeft() {
+        center.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        center.moveRight();
+    }
+
     public String toString(){
-        return String.format("(%1$s, %2$s), speed=(%3$s, %4$s), radius=%4$s" ,
-                center.x, center.y, center.xSpeed,center.ySpeed, radius1);}
-
-
-    public void moveUp() {center.moveUp();
-
-    }
-
-
-    public void moveDown() {center.moveDown();
-
-    }
-
-
-    public void moveLeft() {center.moveLeft();
-
-    }
-
-
-    public void moveRight() {center.moveRight();
-
+        return "("+center.x + "," + center.y + "), speed =("+center.xSpeed + ","+center.ySpeed+"), radius = "+radius;
     }
 }
